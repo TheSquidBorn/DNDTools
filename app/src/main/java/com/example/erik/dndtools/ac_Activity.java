899d36a3c22ccf56a63863dc513a688b841c5212
TheSquidBorn <erik.winnerstam@gmail.com>
2018-04-23 23:32:02 +0200

    Added MainActivity, shell of settings and ac.

---
+++ app/src/main/java/com/example/erik/dndtools/ac_Activity.java
@@ -0,0 +1,13 @@
+package com.example.erik.dndtools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ac_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac_);
+    }
+}
