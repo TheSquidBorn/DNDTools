a183161ed855b2483318bf4259acd8517abab246
TheSquidBorn <erik.winnerstam@gmail.com>
2018-04-29 22:05:55 +0200

    NEVER SPLIT THE PARTY

--- app/src/main/java/com/example/erik/dndtools/MainActivity.java
+++ app/src/main/java/com/example/erik/dndtools/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.erik.dndtools;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
         Button ArmyTool = findViewById(R.id.armyToolButton);
         Button SaveTool = findViewById(R.id.armySaveButton);
+        Button SplitTool = findViewById(R.id.splitButton);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.neversplit);
 
         ArmyTool.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent startIntent = new Intent(getApplicationContext(), ArmySave.class);
                 startActivity(startIntent);
+            }
+        });
+
+        SplitTool.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mp.start();
+
             }
         });
      }
